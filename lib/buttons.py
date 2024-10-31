1b574787eebc980817a3a2e5894084fc1d40f774
Tijs van Erp <tijs@vanerp.io>
2024-10-31 18:06:54 +0100

    removed redundant comments

--- lib/buttons.py
+++ lib/buttons.py
@@ -3,14 +3,6 @@ import board
 import digitalio
 import microcontroller
 
-# KEY_UP = Pin(2,Pin.IN,Pin.PULL_UP)
-# KEY_DOWN = Pin(18,Pin.IN,Pin.PULL_UP)
-# KEY_LEFT= Pin(16,Pin.IN,Pin.PULL_UP)
-# KEY_RIGHT= Pin(20,Pin.IN,Pin.PULL_UP)
-# KEY_CTRL=Pin(3,Pin.IN,Pin.PULL_UP)
-# KEY_A=Pin(15,Pin.IN,Pin.PULL_UP)
-# KEY_B=Pin(17,Pin.IN,Pin.PULL_UP)
-
 
 def set_button(pin: microcontroller.Pin, pull: digitalio.Pull) -> digitalio.DigitalInOut:
     btn = digitalio.DigitalInOut(pin)
