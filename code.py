6660a5199a8fd21e6a73e2e5b58e0c195e08ddee
Tijs van Erp <tijs@vanerp.io>
2024-10-31 19:36:58 +0100

    added comments and removed boot.py as it isn't being used

--- code.py
+++ code.py
@@ -8,10 +8,18 @@ from lib.adafruit_hid.keycode import Keycode
 from lib.adafruit_hid.mouse import Mouse
 
 
-def click(mouse):
+def click(mouse: Mouse) -> None:
+    """
+    Generic left button click function
+    :param mouse: mouse HID device
+    """
     mouse.click(Mouse.LEFT_BUTTON)
 
-def send_w_s_keys(keyboard):
+def send_w_s_keys(keyboard: Keyboard) -> None:
+    """
+    Send w, pause, send s to move forward and back in a game
+    :param keyboard: keyboard HID device
+    """
     keyboard.send(Keycode.W)
     time.sleep(0.5)
     keyboard.send(Keycode.S)
@@ -20,9 +28,11 @@ if __name__ == '__main__':
 
     print("Klikspaan 1.0 booting...")
 
+    # Initialise the HID devices
     kbd = Keyboard(usb_hid.devices)
     m = Mouse(usb_hid.devices)
 
+    # Bind the input buttons
     buttons = MenuButtons(
         up=b.KEY_UP,
         down=b.KEY_DOWN,
@@ -30,6 +40,7 @@ if __name__ == '__main__':
         ok=b.KEY_A
     )
 
+    # Create the user interface menu with the correct font
     menu = Menu(display=display, font="/assets/ib8x16u.bdf", buttons=buttons)
     menu.set_items([
         MenuItem(text="W/S (60s)", active=True, cb=lambda: send_w_s_keys(kbd), interval=30000),
@@ -37,8 +48,12 @@ if __name__ == '__main__':
         MenuItem(text="Muisklik (1s)", active=False, cb=lambda: click(m), interval=1000),
     ])
 
+    # display the menu on the display
     menu.display()
 
     while True:
+        # listen to button presses and perform actions
         menu.listen()
+
+        # let's throttle a bit
         time.sleep(.1)
--- lib/menu.py
+++ lib/menu.py
@@ -1,16 +1,20 @@
-from adafruit_st7735r import ST7735R
+import time
+
+import digitalio
 import displayio
+
 from lib.adafruit_bitmap_font import bitmap_font
 from lib.adafruit_display_text import label
-import time
-import terminalio
+from busdisplay import BusDisplay
 
 COLOR = 0xFFFFFF
 ACTIVE_COLOR = 0xFFFF00
 RUNNING_COLOR = 0xF00FF
 
+
 class MenuButtons:
-    def __init__(self, up, down, cancel, ok):
+    def __init__(self, up: digitalio.DigitalInOut, down: digitalio.DigitalInOut, cancel: digitalio.DigitalInOut,
+                 ok: digitalio.DigitalInOut):
         self.cancel = cancel
         self.down = down
         self.up = up
@@ -22,11 +26,17 @@ class MenuItem:
         self.cb = cb
         self.active = active
         self.text = text
-        self.interval = interval #ms
+        self.interval = interval  # ms
 
 
 class Menu:
-    def __init__(self, display: ST7735R, font: str, buttons: MenuButtons):
+    def __init__(self, display: BusDisplay, font: str, buttons: MenuButtons):
+        """
+
+        :param display: generic display object
+        :param font: binary font for the menu
+        :param buttons: button configuration
+        """
         self._font = bitmap_font.load_font(font)
         self._display = display
         self._items = []
@@ -55,12 +65,22 @@ class Menu:
                 "previous": False
             },
         }
+        # create a blank display canvas
         self.clear()
 
-    def set_items(self, items: []):
+    def set_items(self, items: [MenuItem]) -> None:
+        """
+        Specify the list of menu items
+        :param items:
+        """
         self._items = items
 
-    def display_row(self, item: MenuItem, row_nr: int):
+    def display_row(self, item: MenuItem, row_nr: int) -> None:
+        """
+        Display a single line of the menu. Active and running colors can be specified in a constant
+        :param item: The menu item configuration
+        :param row_nr: The index of the menu row (to track active state)
+        """
         y = (row_nr + 1) * 20
 
         text = f"  {item.text}"
@@ -77,17 +97,26 @@ class Menu:
         text_area.y = y
         self._display.root_group.append(text_area)
 
-    def reset(self):
+    def reset(self) -> None:
+        """
+        Clear the display and set to initial state
+        """
         self.index = 0
         self.current_action = None
         self.clear()
         self.display()
 
-    def clear(self):
+    def clear(self) -> None:
+        """
+        Clear the display
+        """
         splash = displayio.Group()
         self._display.root_group = splash
 
-    def display(self):
+    def display(self) -> None:
+        """
+        Draw the current configuration on the display
+        """
         row_nr = 0
         for idx, i in enumerate(self._items):
             self._items[idx].active = False
@@ -96,10 +125,17 @@ class Menu:
             self.display_row(i, row_nr)
             row_nr = row_nr + 1
 
-    def get_current_action(self):
+    def get_current_action(self) -> (int, callable):
+        """
+        Get the current active menu item and corresponding callback
+        :return: interval of callback plus callback
+        """
         return self._items[self.index].interval, self._items[self.index].cb
 
-    def run_action(self):
+    def run_action(self) -> None:
+        """
+        Run the active callback for the selected menu item on given intervals
+        """
         if self.current_action is not None:
             if self.current_action_last_run is None:
                 self.current_action()
@@ -111,41 +147,56 @@ class Menu:
                     self.current_action_last_run = self.ns_to_ms(time.monotonic_ns())
 
     @staticmethod
-    def ns_to_ms(ns):
+    def ns_to_ms(ns) -> int:
+        """
+        Convenience method to calculate the number of milliseconds for a nanosecond
+        :param ns: nanoseconds
+        :return: number of milliseconds
+        """
         return ns / 1_000_000
 
-    def listen(self):
+    def listen(self) -> None:
+        """
+        Listen to button presses and perform corresponding actions
+        """
 
+        # cancel any running action and return menu to initial state
         self.button_states["cancel"]["state"] = not self.buttons.cancel.value
         if self.button_states["cancel"]["state"] == False and self.button_states["cancel"]["previous"] == True:
             print("reset")
             self.reset()
         self.button_states["cancel"]["previous"] = self.button_states["cancel"]["state"]
 
+        # set selected menu item as active and start running corresponding callback
         self.button_states["ok"]["state"] = not self.buttons.ok.value
         if self.button_states["ok"]["state"] == False and self.button_states["ok"]["previous"] == True:
             self.current_action_index = self.index
             self.current_action_interval, self.current_action = self.get_current_action()
         self.button_states["ok"]["previous"] = self.button_states["ok"]["state"]
 
+        # Navigate "down" the menu
         self.button_states["down"]["state"] = not self.buttons.down.value
         if self.button_states["down"]["state"] == False and self.button_states["down"]["previous"] == True:
             self.index = self.index + 1
         self.button_states["down"]["previous"] = self.button_states["down"]["state"]
 
+        # Navigate "up" the menu
         self.button_states["up"]["state"] = not self.buttons.up.value
         if self.button_states["up"]["state"] == False and self.button_states["up"]["previous"] == True:
             self.index = self.index - 1
         self.button_states["up"]["previous"] = self.button_states["up"]["state"]
 
+        # make sure we can cycle the menu when at the end
         if self.index >= len(self._items):
             self.index = 0
         if self.index < 0:
             self.index = len(self._items) - 1
 
+        # only refresh the screen when something meaningful changed
         if self.previous_index != self.index:
             self.clear()
             self.display()
             self.previous_index = self.index
 
+        # run current action
         self.run_action()
